5058a3a1cccfa8232f5183620e2f615c28767f69
RogerLi818 <jackliteng@yahoo.com>
2022-04-28 23:56:59 -0700

    next 9.1.6

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-console.log(document)
+const profileDataArgs=process.argv.slice(2, process.argv.length);
+
+console.log(profileDataArgs);
+console.log(process.argv.length);
+
+const sum =5+3;
+
+sum +=1;
+
+console.log(sum)
