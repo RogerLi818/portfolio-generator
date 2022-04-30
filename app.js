04f16190927f6fc8ece51ad7081179dde0b76679
RogerLi818 <jackliteng@yahoo.com>
2022-04-30 12:41:36 -0700

    http added

--- app.js
+++ app.js
@@ -1,19 +1,30 @@
+const fs = require('fs');
+
+const generatePage=require('./src/page-template')
 const profileDataArgs=process.argv.slice(2, process.argv.length);
 
-console.log(profileDataArgs);
-console.log(process.argv.length);
+const [name, github]=profileDataArgs;
+
+// console.log(profileDataArgs);
+// console.log(process.argv.length);
+
+// const printProfileData=profileDataArr=>{
+//     for (let i =0; i<profileDataArr.length;i++){
+//         console.log(profileDataArr[i]);
+//     }
+
+// console.log('==================================')
+
+// profileDataArr.forEach(profileItem=>console.log(profileItem))
+
+// };
+
+// printProfileData(profileDataArgs)
 
 
-const printProfileData=profileDataArr=>{
-    for (let i =0; i<profileDataArr.length;i++){
-        console.log(profileDataArr[i]);
-    }
 
+fs.writeFile('index.html',generatePage(name,github),err=>{
+    if(err) throw err;
 
-console.log('==================================')
-
-profileDataArr.forEach(profileItem=>console.log(profileItem))
-
-};
-
-printProfileData(profileDataArgs)
+    console.log("Protfolio complete! Check out index.html to see the output!");
+});
